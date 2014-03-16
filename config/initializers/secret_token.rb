376d9c42dfbb99ff2e018fdc5368e3bb6f039658
Abhi Ramesh <abhiramesh@gmail.com>
2014-03-16 13:38:14 -0400

    create git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TennisBook::Application.config.secret_token = '7e5dc4f3ee8bd587c7d7f81bf43b000a7df94ebd6495a2e69e392afd4e8497faebf8b1e9174c1cd08e40cd790517d5b882ff1a269963c909716658e3cf440ab0'
