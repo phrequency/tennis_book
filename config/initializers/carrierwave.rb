3db21888a7d1a82c74b02e3c1f498484ee4281f1
Abhi Ramesh <abhiramesh@gmail.com>
2014-09-22 17:07:47 -0400

    amzn

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: 'AKIAIMGYJXQ76NRH7YTQ',
-    aws_secret_access_key: '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',
+    aws_access_key_id: 'ACCESS ID',
+    aws_secret_access_key: 'ACCESS ID',
     region: 'us-west-2'
   }
   config.fog_directory = 'tennis-book'
